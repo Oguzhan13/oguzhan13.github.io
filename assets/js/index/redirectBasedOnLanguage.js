39288234ca62e13909608b5ca2bcb34d3493ffa2
Oguzhan13 <oguzhanbayram13@gmail.com>
2023-08-19 19:26:31 +0300

    proje mimarisi değiştirildi

--- assets/js/index/redirectBasedOnLanguage.js
+++
@@ -1,9 +0,0 @@
-function redirectBasedOnLanguage(pathTr, pathEn) {
-    var userLang = navigator.language || navigator.userLanguage;
-
-    if (userLang === "tr" || userLang.startsWith("tr")) {
-        window.location.href = pathTr;
-    } else {
-        window.location.href = pathEn;
-    }
-}
