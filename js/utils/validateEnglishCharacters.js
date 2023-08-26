9d912d9c1ea38c4189792eb64148510914c11c4e
Oguzhan13 <oguzhanbayram13@gmail.com>
2023-08-27 02:06:51 +0300

    dosya görüntüleme ve indirme eklendi

--- js/utils/validateEnglishCharacters.js
+++ js/utils/validateEnglishCharacters.js
@@ -3,13 +3,11 @@ export function validateEnglishCharacters(errorMessage) {
     
     textarea.addEventListener("input", function() {
         var inputValue = textarea.value;
-        var englishOnly = /^[A-Za-z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+        var englishOnly = /^[A-Za-z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/? ]*$/;
         
-        if (!englishOnly.test(inputValue)) {
-            // İngilizce karakter dışında karakter bulundu
-            alert(errorMessage);
-            // İstenmeyen karakterleri temizle
-            textarea.value = inputValue.replace(/[^A-Za-z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g, "");
+        if (!englishOnly.test(inputValue)) {            
+            alert(errorMessage);            
+            textarea.value = inputValue.replace(/[^A-Za-z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/? ]/g, "");
         }
     });
 }
